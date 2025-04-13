c82e7121cdd1ceb0e95ee90d0f271f77061efb12
Aymen Hassini <hassiniaymen@yahoo.com>
2025-04-13 11:24:59 +0100

    Person and Employee classes

---
+++ mgdb/src/main/java/com/example/Employee.java
@@ -0,0 +1,32 @@
+package com.example;
+
+public class Employee extends Person {
+
+    String password;
+    String department;
+    double commissionRate;
+
+    public Employee(String fname, String lname, String email, String phoneNumber, String password, String department, double commissionRate) {
+        super(fname, lname, email, phoneNumber);
+        this.password = password;
+        this.department = department;
+        this.commissionRate = commissionRate;
+    }
+
+    public Employee(String id, String fname, String lname, String email, String phoneNumber, String password, String department, double commissionRate) {
+        super(id, fname, lname, email, phoneNumber);
+        this.password = password;
+        this.department = department;
+        this.commissionRate = commissionRate;
+    }
+
+    public String getPassword() { return password; } 
+    public void setPassword(String password) { this.password = password; } 
+    
+    public String getDepartment() { return department; } 
+    public void setDepartment(String department) { this.department = department; } 
+    
+    public double getCommissionRate() { return commissionRate; } 
+    public void setCommissionRate(double commissionRate) { this.commissionRate = commissionRate; }
+    
+}
---
+++ mgdb/src/main/java/com/example/Person.java
@@ -0,0 +1,40 @@
+package com.example;
+
+public class Person {
+    String id;
+    String fname;
+    String lname;
+    String email;
+    String phoneNumber;
+
+    public Person(String fname, String lname, String email, String phoneNumber) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Person(String id, String fname, String lname, String email, String phoneNumber) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getFname() { return fname; }
+    public void setFname(String fname) { this.fname = fname; }
+
+    public String getLname() { return lname; }
+    public void setLname(String lname) { this.lname = lname; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+}
