ea955a584bf6c1662055ce7954ada123babe9b84
Aymen Hassini <hassiniaymen@yahoo.com>
2025-04-14 15:34:41 +0100

    making the Sale class

---
+++ mgdb/src/main/java/com/example/Sale.java
@@ -0,0 +1,81 @@
+package com.example;
+
+public class Sale {
+    String id;
+    String carId;
+    String clientId;
+    String employeeId;
+    Double fullPrice;
+    Double initialDeposit;
+    double interestRate;
+    double monthlyPayment;
+    int leaseDuration;
+    int monthsRemaining;
+    boolean isFullyPaid;
+
+
+    public Sale(String carId, String clientId, String employeeId, Double fullPrice,
+                Double initialDeposit, double interestRate, double monthlyPayment,
+                int leaseDuration, int monthsRemaining, boolean isFullyPaid) {
+        this.carId = carId;
+        this.clientId = clientId;
+        this.employeeId = employeeId;
+        this.fullPrice = fullPrice;
+        this.initialDeposit = initialDeposit;
+        this.interestRate = interestRate;
+        this.monthlyPayment = monthlyPayment;
+        this.leaseDuration = leaseDuration;
+        this.monthsRemaining = monthsRemaining;
+        this.isFullyPaid = isFullyPaid;
+    }
+
+
+    public Sale(String id, String carId, String clientId, String employeeId, Double fullPrice,
+                Double initialDeposit, double interestRate, double monthlyPayment,
+                int leaseDuration, int monthsRemaining, boolean isFullyPaid) {
+        this.id = id;
+        this.carId = carId;
+        this.clientId = clientId;
+        this.employeeId = employeeId;
+        this.fullPrice = fullPrice;
+        this.initialDeposit = initialDeposit;
+        this.interestRate = interestRate;
+        this.monthlyPayment = monthlyPayment;
+        this.leaseDuration = leaseDuration;
+        this.monthsRemaining = monthsRemaining;
+        this.isFullyPaid = isFullyPaid;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getCarId() { return carId; }
+    public void setCarId(String carId) { this.carId = carId; }
+
+    public String getClientId() { return clientId; }
+    public void setClientId(String clientId) { this.clientId = clientId; }
+
+    public String getEmployeeId() { return employeeId; }
+    public void setEmployeeId(String employeeId) { this.employeeId = employeeId; }
+
+    public Double getFullPrice() { return fullPrice; }
+    public void setFullPrice(Double fullPrice) { this.fullPrice = fullPrice; }
+
+    public Double getInitialDeposit() { return initialDeposit; }
+    public void setInitialDeposit(Double initialDeposit) { this.initialDeposit = initialDeposit; }
+
+    public double getInterestRate() { return interestRate; }
+    public void setInterestRate(double interestRate) { this.interestRate = interestRate; }
+
+    public double getMonthlyPayment() { return monthlyPayment; }
+    public void setMonthlyPayment(double monthlyPayment) { this.monthlyPayment = monthlyPayment; }
+
+    public int getLeaseDuration() { return leaseDuration; }
+    public void setLeaseDuration(int leaseDuration) { this.leaseDuration = leaseDuration; }
+
+    public int getMonthsRemaining() { return monthsRemaining; }
+    public void setMonthsRemaining(int monthsRemaining) { this.monthsRemaining = monthsRemaining; }
+
+    public boolean isFullyPaid() { return isFullyPaid; }
+    public void setFullyPaid(boolean fullyPaid) { isFullyPaid = fullyPaid; }
+}
